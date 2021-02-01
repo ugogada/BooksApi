1c190de2df5f0242bf5e7950f9b045d90e06c77a
ugogada <ugogada251@rku.ac.in>
2021-02-01 21:45:09 +0530

    added port, start script, connection string

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 const route = require('./route');
 
 // connecting to mongodb
-mongoose.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true,useUnifiedTopology: true}).then(
+mongoose.connect("mongodb+srv://Uvesh:Uvesh@student.oqnzh.mongodb.net/Student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology: true}).then(
     ()=>{
         const app = express();
         app.use("/api",route);
-        app.listen(3000,()=>{
+        app.listen(process.env.PORT || 3000,()=>{
             console.log("Server Started...!");
         })
     }
